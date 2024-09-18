ce3d05c6b901947a1761ec9ccd50d7fdf4c7bddd
Olakunle Hassan <kunlex_042003@yahoo.com>
2024-09-18 12:36:57 +0100

    fix: updated my public resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -89,8 +89,8 @@ const AppBanner = () => {
             className="flex justify-center sm:block"
           >
             <a
-              download="Olakunle-Hassan.pdf"
-              href="/files/Olakunle-Hassan.pdf"
+              download="Olakunle_Hassan.pdf"
+              href="/files/Olakunle_Hassan.pdf"
               className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
               aria-label="Download Resume"
             >
